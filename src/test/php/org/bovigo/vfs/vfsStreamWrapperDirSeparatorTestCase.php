daa40d42ba30e3f63a9af59f864059f4a6ed7238
Frank Kleine <mikey@stubbles.net>
2011-07-11 22:11:20 +0200

    add test case for #8: doesn't support windows directory separator

---
+++ src/test/php/org/bovigo/vfs/vfsStreamWrapperDirSeparatorTestCase.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Test that using windows directory separator works correct.
+ *
+ * @package     bovigo_vfs
+ * @subpackage  test
+ */
+require_once 'org/bovigo/vfs/vfsStream.php';
+require_once 'PHPUnit/Framework.php';
+/**
+ * Test that using windows directory separator works correct.
+ *
+ * @package     bovigo_vfs
+ * @subpackage  test
+ * @group       issue_8
+ * @since       0.9.0
+ */
+class vfsStreamWrapperDirSeparatorTestCase extends PHPUnit_Framework_TestCase
+{
+    /**
+     * root diretory
+     *
+     * @var  vfsStreamDirectory
+     */
+    protected $root;
+
+    /**
+     * set up test environment
+     */
+    public function setUp()
+    {
+        $this->root = vfsStream::setup();
+    }
+
+    /**
+     * @test
+     */
+    public function fileCanBeAccessedUsingWinDirSeparator()
+    {
+        vfsStream::newFile('foo/bar/baz.txt')
+                 ->at($this->root)
+                 ->withContent('test');
+        $this->assertEquals('test', file_get_contents('vfs://root/foo\bar\baz.txt'));
+    }
+
+
+    /**
+     * @test
+     */
+    public function directoryCanBeCreatedUsingWinDirSeparator()
+    {
+        mkdir('vfs://root/dir\bar\foo', true, 0777);
+        $this->assertTrue($this->root->hasChild('dir'));
+        $this->assertTrue($this->root->getChild('dir')->hasChild('bar'));
+        $this->assertTrue($this->root->getChild('dir/bar')->hasChild('foo'));
+    }
+}
+?>
