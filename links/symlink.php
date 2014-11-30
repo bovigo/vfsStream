27977aff2cfc12beb2f5a1da13c41a8758e54f46
mikey179 <mikey@stubbles.net>
2014-11-30 20:03:33 +0100

    add example for symlink()

---
+++ links/symlink.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * This file is part of vfsStream.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @package  org\bovigo\vfs
+ */
+namespace org\bovigo\vfs\linking;
+
+require 'bootstrap.php';
+
+use org\bovigo\vfs\vfsStream;
+
+$root = vfsStream::setup();
+$dir  = vfsStream::newDirectory('some')->at($root);
+$file = vfsStream::newFile('target.txt')->withContent('hello, world!')->at($dir);
+
+var_dump(symlink($file->url(), 'testfoo'));
